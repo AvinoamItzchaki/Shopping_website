6802945ff9292d5a3a2100d14b41dd2f9755132e
AvinoamItzchaki <avinoamm2007@gmail.com>
2025-08-06 10:31:38 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,7 @@ function About(){
     return (
         <div className="about">
             <h1 className="title">שלום וברכה למשתמש {username}!</h1>
-            <br/><br/>
-            <h2 className="h2">האתר נבנה על ידי אבינעם יצחקי בכיתה יב 2 למען פרויקט גמר במגמת מדעי המחשב בישיבת צביה
-                אשקלון</h2>
+            
             <br/><br/><br/><br/><br/><br/><br/>
 
             <h1 className="end">בילוי נעים באתר!</h1>
